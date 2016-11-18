2a916ca8d7ed9ef91a5f60707f535cc10a4a4961
Andy Sartori <Andy@AndyMBP.local>
2016-11-18 13:58:39 -0800

    Pseudo code and bare bones

---
+++ js.js
@@ -0,0 +1,14 @@
+console.log('hello');
+
+      // make clear button clear board // event handler clear all the divs
+      // click drop token and lower dot // when clicked will append a green div
+      // first token dropped is green, then red, then green keeps alternating
+
+      // set winning conditions.
+      //If there are 4 greens in a horizontal row // if else statements
+      //If there are 4 greens in a diagonal row // if else statements
+      //If there are 4 greens in a vertical row // if else statements
+      or
+      //If there are 4 reds in a horizontal row // if else statements
+      //If there are 4 reds in a diagonal row // if esle statements
+      //If there are 4 reds in a vertical row // if else statements
