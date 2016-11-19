d99dc1926d091ca09164e7c707e988b95cffc914
Andy Sartori <Andy@AndyMBP.local>
2016-11-19 10:12:40 -0800

    first js functionality

--- js.js
+++ js.js
@@ -1,14 +1,25 @@
 console.log('hello');
 
       // make clear button clear board // event handler clear all the divs
-      // click drop token and lower dot // when clicked will append a green div
+
+
+      var button = document.querySelector('#column1');  // click drop token and lower dot // when clicked will append a green div
+      var thirtySix = document.querySelector('#thirtySix');
+      console.log(button);
+      var doSomeStuff = function() {
+      	var color = thirtySix.classList.add('green');
+      	}
+      button.addEventListener('click', doSomeStuff);
+
+      // next Sequential click will be opposite color // use toggle
+
       // first token dropped is green, then red, then green keeps alternating
 
       // set winning conditions.
       //If there are 4 greens in a horizontal row // if else statements
       //If there are 4 greens in a diagonal row // if else statements
       //If there are 4 greens in a vertical row // if else statements
-      or
+      //or
       //If there are 4 reds in a horizontal row // if else statements
       //If there are 4 reds in a diagonal row // if esle statements
       //If there are 4 reds in a vertical row // if else statements
