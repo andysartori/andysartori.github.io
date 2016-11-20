03a4a6e11d889340f63f8e01d2666a3f7b3a2287
Andy Sartori <Andy@AndyMBP.local>
2016-11-20 10:36:15 -0800

    js for columns

--- js.js
+++ js.js
@@ -1,25 +1,77 @@
 console.log('hello');
 
-      // make clear button clear board // event handler clear all the divs
+// stores state of the board
+  var column1 = ['blank', 'blank', 'blank', 'blank', 'blank', 'blank'];
+  var column2 = ['blank', 'blank', 'blank', 'blank', 'blank', 'blank'];
+  var divsColumn1 = ['r1c1', 'r2c1', 'r3c1', 'r4c1', 'r5c1', 'r6c1',];
+  var divsColumn2 = ['r1c2', 'r2c2', 'r3c2', 'r4c2', 'r5c2', 'r6c2',];
+
+  var turn = true;     // Telling computer that to start variable is equal to true
+
+  /*When button is clicked check the value of turn */
+  var button1 = document.querySelector('#token1');
+  var button2 = document.querySelector('#token2');
 
 
-      var button = document.querySelector('#column1');  // click drop token and lower dot // when clicked will append a green div
-      var thirtySix = document.querySelector('#thirtySix');
-      console.log(button);
-      var doSomeStuff = function() {
-      	var color = thirtySix.classList.add('green');
-      	}
-      button.addEventListener('click', doSomeStuff);
 
-      // next Sequential click will be opposite color // use toggle
 
-      // first token dropped is green, then red, then green keeps alternating
+  console.log(button1);
+  var column1buttonpushed = function() {
+    for (var num = 5; num > -1; num--) {
+      if (column1[num] == 'blank') {
+        console.log(num)
+        if (turn == true){
+          document.getElementById(divsColumn1[num]).setAttribute('class', 'green');
+          column1[num] = 'green';
+        }
+        else {
+          document.getElementById(divsColumn1[num]).setAttribute('class', 'red');
+          column1[num] = 'red';
+        }
+        /*Flipping the value of Turn*/
+        turn = !turn;
+        return
+      }
+    }
+  }
 
-      // set winning conditions.
-      //If there are 4 greens in a horizontal row // if else statements
-      //If there are 4 greens in a diagonal row // if else statements
-      //If there are 4 greens in a vertical row // if else statements
-      //or
-      //If there are 4 reds in a horizontal row // if else statements
-      //If there are 4 reds in a diagonal row // if esle statements
-      //If there are 4 reds in a vertical row // if else statements
+
+
+  console.log(button2);
+  var column2buttonpushed = function() {
+    for (var num = 5; num > -1; num--) {
+      if (column2[num] == 'blank') {
+        console.log(num)
+        if (turn == true){
+          document.getElementById(divsColumn2[num]).setAttribute('class', 'green');
+          column2[num] = 'green';
+        }
+        else {
+          document.getElementById(divsColumn2[num]).setAttribute('class', 'red');
+          column2[num] = 'red';
+        }
+        /*Flipping the value of Turn*/
+        turn = !turn;
+        return
+      }
+    }
+  }
+
+  button1.addEventListener('click', column1buttonpushed);
+  button2.addEventListener('click', column2buttonpushed);
+
+
+
+
+
+
+  // first token dropped is green, then red, then green keeps alternating
+
+  // set winning conditions.
+  //If there are 4 greens in a horizontal row // if else statements
+  //If there are 4 greens in a diagonal row // if else statements
+  //If there are 4 greens in a vertical row // if else statements
+  //or
+  //If there are 4 reds in a horizontal row // if else statements
+  //If there are 4 reds in a diagonal row // if esle statements
+  //If there are 4 reds in a vertical row // if else statements
