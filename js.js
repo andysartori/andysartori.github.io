f01a2dd4ea8b91d158ca87165f41656799354233
Andy Sartori <Andy@AndyMBP.local>
2016-11-27 20:37:38 -0800

    game with winning conditions

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-console.log('hello');
+// console.log('hello');
 
 // stores state of the board
   var column1 = ['blank', 'blank', 'blank', 'blank', 'blank', 'blank'];
@@ -15,12 +15,15 @@ console.log('hello');
   var divsColumn5 = ['r1c5', 'r2c5', 'r3c5', 'r4c5', 'r5c5', 'r6c5',];
   var divsColumn6 = ['r1c6', 'r2c6', 'r3c6', 'r4c6', 'r5c6', 'r6c6',];
   var divsColumn7 = ['r1c7', 'r2c7', 'r3c7', 'r4c7', 'r5c7', 'r6c7',];
+  var allcolumns = [ column1, column2, column3, column4, column5, column6, column7];
+  var alldivs = [divsColumn1, divsColumn2, divsColumn3, divsColumn4, divsColumn5, divsColumn6, divsColumn7];
 
+  var won = false;     // nobody won yet
   var turn = true;     // Telling computer that to start variable is equal to true
 
   /*When button is clicked check the value of turn */
   var button0 = document.querySelector('#token0'); // clear button
-  var button1 = document.querySelector('#token1');
+  var button1 = document.querySelector('#token1'); // button row 1column1
   var button2 = document.querySelector('#token2');
   var button3 = document.querySelector('#token3');
   var button4 = document.querySelector('#token4');
@@ -28,176 +31,95 @@ console.log('hello');
   var button6 = document.querySelector('#token6');
   var button7 = document.querySelector('#token7');
 
+  // var verticalWinningCondition = function () {
+  //   for (var num = 0; num < column1.length; num++){
+  //     if ((column1[num] && column1[num+1] && column1[num+2] && column1[num+3])!= 'blank'){
+  //       console.log('winner', turn);
+  //     }
+  //   }
+  // }
+
+  var horizontalWinningCondition = function () {
+    for (var row = 0; row < column1.length ; row++){  // all columns are same so use column1
+      for (var col = 0; col < allcolumns.length - 3; col++) { //do not check outside board
+       if (allcolumns[col][row] === 'green'  && allcolumns[col+1][row] === 'green'  && allcolumns[col+2][row] === 'green'  && allcolumns[col+3][row] === 'green') { // check 4 consecutive places
+         console.log('green won');
+         won = true;
+       } else if (allcolumns[col][row] === 'red'  && allcolumns[col+1][row] === 'red'  && allcolumns[col+2][row] === 'red'  && allcolumns[col+3][row] === 'red'){
+         console.log('red won');
+         won = true;
+       }
+      }
+    }
+  }
+
   var verticalWinningCondition = function () {
-    for (var num = 0; num < column1.length; num++){
-      if (column1[num] && column1[num+1] && column1[num] != 'blank'){
-        console.log('winner', turn);
-      }
-    }
-  }
-
-
-  console.log(button0);
-  var clearbuttonpushed = function(){
-    document.querySelector(".round").reset(); // set arrays back to blank
-  }
-
-  console.log(button1);
-  var column1buttonpushed = function() {
-    for (var num = 5; num > -1; num--) {
-      if (column1[num] == 'blank') {
-        console.log(num)
-        if (turn == true){
-          document.getElementById(divsColumn1[num]).setAttribute('class', 'green');
-          column1[num] = 'green';
-        }
-        else {
-          document.getElementById(divsColumn1[num]).setAttribute('class', 'red');
-          column1[num] = 'red';
-        }
-        verticalWinningCondition();
-        turn = !turn; // flip value turn
-        return
+    for (var row = 0; row < column1.length ; row++){  // all columns are same so use column1
+      for (var col = 0; col < allcolumns.length - 3; col++) {
+       if (allcolumns[col][row] === 'green'  && allcolumns[col][row+1] === 'green'  && allcolumns[col][row+2] === 'green'  && allcolumns[col][row+3] === 'green') {
+         console.log('green won');
+         won = true;
+       } else if (allcolumns[col][row] === 'red'  && allcolumns[col][row+1] === 'red'  && allcolumns[col][row+2] === 'red'  && allcolumns[col][row+3] === 'red'){
+         console.log('red won');
+         won = true;
+       }
       }
     }
   }
 
 
 
-  console.log(button2);
-  var column2buttonpushed = function() {
-    for (var num = 5; num > -1; num--) {
-      if (column2[num] == 'blank') {
-        console.log(num)
-        if (turn == true){
-          document.getElementById(divsColumn2[num]).setAttribute('class', 'green');
-          column2[num] = 'green';
+  // console.log(button0);
+  var clearbuttonpushed = function(){ // re-start game button
+    won = false; // set value back to nobody won
+    allcolumns.forEach(function(element){
+      element.fill('blank'); //put back blank
+    });
+    alldivs.forEach(function(div){
+      div.forEach(function(id){
+        document.getElementById(id).setAttribute('class', 'transparent'); // remove color visual from board
+      });
+    });
+  };
+
+  // console.log(button1);
+  var columnbuttonpushed = function() {    // when button is pushed function is called
+    if (!won) {
+      var buttonNumber = event.target.id.replace("token","") -1 ;  //
+              // iterate over column array of the button that was pushed
+      //console.log(event.target.id);           // event.target.id is a string typeof
+      for (var posColChecking = 5; posColChecking > -1; posColChecking--) { // stepping through array to find empty values
+        if (allcolumns[buttonNumber][posColChecking] == 'blank') { // allclumns is the outmost array -  buttonNumber used to represent column array . check position to see if it is blank. if it is blank that is where we want to fill in a token
+          // console.log(posColChecking)
+          if (turn == true){
+            document.getElementById(alldivs[buttonNumber][posColChecking]).setAttribute('class', 'green'); // get id of th element
+            allcolumns[buttonNumber][posColChecking] = 'green'; // setting the array of our column to green
+          }
+          else {
+            document.getElementById(alldivs[buttonNumber][posColChecking]).setAttribute('class', 'red');
+            allcolumns[buttonNumber][posColChecking] = 'red';
+          }
+          horizontalWinningCondition(); // checks if someone won every turn
+          verticalWinningCondition();
+          turn = !turn; // flip value turn
+          return
         }
-        else {
-          document.getElementById(divsColumn2[num]).setAttribute('class', 'red');
-          column2[num] = 'red';
-        }
-        verticalWinningCondition();
-        turn = !turn; // flip value turn
-        return
       }
     }
   }
 
-
-  console.log(button3);
-  var column3buttonpushed = function() {
-    for (var num = 5; num > -1; num--) {
-      if (column3[num] == 'blank') {
-        console.log(num)
-        if (turn == true){
-          document.getElementById(divsColumn3[num]).setAttribute('class', 'green');
-          column3[num] = 'green';
-        }
-        else {
-          document.getElementById(divsColumn3[num]).setAttribute('class', 'red');
-          column3[num] = 'red';
-        }
-        verticalWinningCondition();
-        turn = !turn; // flip value turn
-        return
-      }
-    }
-  }
-
-  console.log(button4);
-  var column4buttonpushed = function() {
-    for (var num = 5; num > -1; num--) {
-      if (column4[num] == 'blank') {
-        console.log(num)
-        if (turn == true){
-          document.getElementById(divsColumn4[num]).setAttribute('class', 'green');
-          column4[num] = 'green';
-        }
-        else {
-          document.getElementById(divsColumn4[num]).setAttribute('class', 'red');
-          column4[num] = 'red';
-        }
-        verticalWinningCondition();
-        turn = !turn; // flip value turn
-        return
-      }
-    }
-  }
-
-  console.log(button5);
-  var column5buttonpushed = function() {
-    for (var num = 5; num > -1; num--) {
-      if (column5[num] == 'blank') {
-        console.log(num)
-        if (turn == true){
-          document.getElementById(divsColumn5[num]).setAttribute('class', 'green');
-          column5[num] = 'green';
-        }
-        else {
-          document.getElementById(divsColumn5[num]).setAttribute('class', 'red');
-          column5[num] = 'red';
-        }
-        verticalWinningCondition();
-        turn = !turn; // flip value turn
-        return
-      }
-    }
-  }
-
-
-  console.log(button6);
-  var column6buttonpushed = function() {
-    for (var num = 5; num > -1; num--) {
-      if (column6[num] == 'blank') {
-        console.log(num)
-        if (turn == true){
-          document.getElementById(divsColumn6[num]).setAttribute('class', 'green');
-          column6[num] = 'green';
-        }
-        else {
-          document.getElementById(divsColumn6[num]).setAttribute('class', 'red');
-          column6[num] = 'red';
-        }
-        verticalWinningCondition();
-        turn = !turn; // flip value turn
-        return
-      }
-    }
-  }
-
-
-  console.log(button7);
-  var column7buttonpushed = function() {
-    for (var num = 5; num > -1; num--) {
-      if (column7[num] == 'blank') {
-        console.log(num)
-        if (turn == true){
-          document.getElementById(divsColumn7[num]).setAttribute('class', 'green');
-          column7[num] = 'green';
-        }
-        else {
-          document.getElementById(divsColumn7[num]).setAttribute('class', 'red');
-          column7[num] = 'red';
-        }
-        verticalWinningCondition();
-        turn = !turn; // flip value turn
-        return
-      }
-    }
-  }
 
 
 
 
   button0.addEventListener('click', clearbuttonpushed);
-  button1.addEventListener('click', column1buttonpushed);
-  button2.addEventListener('click', column2buttonpushed);
-  button3.addEventListener('click', column3buttonpushed);
-  button4.addEventListener('click', column4buttonpushed);
-  button5.addEventListener('click', column5buttonpushed);
-  button6.addEventListener('click', column6buttonpushed);
-  button7.addEventListener('click', column7buttonpushed);
+  button1.addEventListener('click', columnbuttonpushed); // column1buttonpushed is a callback function
+  button2.addEventListener('click', columnbuttonpushed);
+  button3.addEventListener('click', columnbuttonpushed);
+  button4.addEventListener('click', columnbuttonpushed);
+  button5.addEventListener('click', columnbuttonpushed);
+  button6.addEventListener('click', columnbuttonpushed);
+  button7.addEventListener('click', columnbuttonpushed);
 
 
 
